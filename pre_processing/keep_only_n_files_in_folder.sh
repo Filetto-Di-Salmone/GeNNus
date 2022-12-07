2c4c0a1f4cf660381c420e5baba71d5a3a50049b
Daniele Solombrino <daniele.solombrino@gmail.com>
2022-12-07 21:14:19 +0100

    improve determinism in file selection

--- pre_processing/keep_only_n_files_in_folder.sh
+++ pre_processing/keep_only_n_files_in_folder.sh
@@ -1,3 +1,3 @@
 cd Electronic
 
-find . -maxdepth 1 -type f -name "*.mp3" -print0 | head -z -n 2000 | xargs -0 rm 
+find . -maxdepth 1 -type f -name "*.mp3" -print0 | sort -z | head -z -n 2000 | xargs -0 rm
